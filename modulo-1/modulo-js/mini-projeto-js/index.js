dabed02eeb3ee61f00cbf970c91ce7077460a95c
Gabriel Costa <gabcostta37@gmail.com>
2024-07-01 11:35:37 -0300

    feat: adicionar funcionalidade de busca de dados de endereço pelo CEP e preencher campos, além de adicionar seleção de regiões, estados e cidades

--- modulo-1/modulo-js/mini-projeto-js/index.js
+++ modulo-1/modulo-js/mini-projeto-js/index.js
@@ -1,1 +1,50 @@
-localStorage.setItem("nome", "Gabriel");
+cep.onkeyup = () => {
+    if (cep.value.length ==8) {
+        fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(resposta => resposta.json())
+        .then(resposta2 => {
+            rua.value = resposta2.logradouro;
+            bairro.value = resposta2.bairro;
+            cidade.value = resposta2.localidade;
+            estado.value = resposta2.uf;
+        })
+        .finally(() => {
+            alert("Procedimento concluido");
+        })
+    }   
+}
+
+function buscarRegioes(){
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes")
+    .then(res => res.json())
+    .then(res => {
+        res.map(regiao => {
+            regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`;
+        })
+    })
+}
+
+buscarRegioes();
+
+function buscarEstados(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`)
+    .then(res => res.json())
+    .then(res => {
+        estados.innerHTML = "";
+        res.map(estado => {
+            estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+        })
+    })
+}
+
+function buscarCidades(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
+`) // alterar para buscar por uf cidade.
+    .then(res => res.json())
+    .then(res => {
+        estados.innerHTML = "";
+        res.map(estado => {
+            estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+        })
+    })
+}
