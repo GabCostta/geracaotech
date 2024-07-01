60352c89b322b35d44d299215340879829ecae51
GabCostta <gabcostta37@gmail.com>
2024-07-01 15:26:56 -0300

    feat: Finalizando material solicitado aula: -Foi adicionado a busca por cep atraves da api (Viacep). -Foi adicionado a seleção de regioes,estados,cidades atraves da api (ibge api).

--- modulo-1/modulo-js/api/index.js
+++ modulo-1/modulo-js/api/index.js
@@ -14,7 +14,7 @@ cep.onkeyup = () => {
     }   
 }
 
-function buscarRegioes(){
+function buscarRegioes() {
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes")
     .then(res => res.json())
     .then(res => {
@@ -26,25 +26,26 @@ function buscarRegioes(){
 
 buscarRegioes();
 
-function buscarEstados(){
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`)
+function buscarEstados() {
+    const regiaoSelecionada = document.getElementById('regioes').value;
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regiaoSelecionada}/estados`)
     .then(res => res.json())
     .then(res => {
-        estados.innerHTML = "";
+        estados.innerHTML = '<option value="">Selecione um Estado</option>';
         res.map(estado => {
             estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
         })
     })
 }
 
-function buscarCidades(){
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
-`) // alterar para buscar por uf cidade.
+function buscarCidades() {
+    const estadoSelecionado = document.getElementById('estados').value;
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios?orderBy=nome`)
     .then(res => res.json())
     .then(res => {
-        estados.innerHTML = "";
-        res.map(estado => {
-            estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+        cidades.innerHTML = '<option value="">Selecione uma Cidade</option>';
+        res.map(cidade => {
+            cidades.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`;
         })
     })
 }
